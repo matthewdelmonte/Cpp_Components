c8e40aeaddb90df885c8f3a1a15f6b5a184840e0
Matthew Del_Monte <mdel3089@gmail.com>
2018-08-31 12:17:21 -0400

    Add files via upload

---
+++ circleExercise.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int radius;
+	//Request input to determine Area and Circumference of a circle
+	cout << " Let's determine the Area and Circumference of a circle. " << endl;
+
+	//Variable to store radius	
+	cout << " Enter a radius: ";
+	cin >> radius;
+
+	//Calculate the area of a circle
+	//A = Pi * r * r
+	cout << " The Area of the circle = " << 3.14*radius*radius << endl;
+	
+
+	//Calculate the circumference of a circle
+	//C = 2 * Pi * r
+	cout << " The Circumference of the circle = " << 2*3.14*radius << endl;
+
+	return 0;
+}
