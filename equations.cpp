0f49232d27c8089a95f6e947e5a3288537e9b07a
Matthew Del_Monte <mdel3089@gmail.com>
2018-09-21 14:41:33 -0400

    Add files via upload

---
+++ equations.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+//Input first integer
+	int numb1 = 0;
+	cout << " Enter first number ";
+	cin >> numb1;
+
+//Input second integer
+	int numb2 = 0;
+	cout << " Enter second number ";
+	cin >> numb2;
+
+//Input arithemtic symbol
+	char userSelection = '\0';
+	cout << " Enter s = subtract, m = multiply, d = divide, anything else to add " << endl;
+	cin >> userSelection;
+
+//If then statement
+	int result = 0;
+	if (userSelection == 's')
+	result = numb1 - numb2;
+	else if
+	(userSelection == 'm')
+	result = numb1 * numb2;
+	else if
+	(userSelection == 'd')
+	result = numb1 / numb2;
+	else
+	result = numb1 + numb2;
+
+	cout << " The answer is " << result << endl;
+
+	return 0;
+}
